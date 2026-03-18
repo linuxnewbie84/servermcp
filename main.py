45fbd92329f88278508771a89ccca2195f326776
linuxnewbie84 <68965124+linuxnewbie84@users.noreply.github.com>
2026-03-18 14:24:49 -0600

    Realizados los cambios de main

--- main.py
+++ main.py
@@ -3,3 +3,19 @@ from datetime import datetime
 from typing import List, Dict, Any, Optional
 import psycopg2
 from psycopg2.extras import RealDictCursor
+from fastmcp import FastMCP
+
+mcp = FastMCP('connection')
+mcp.title= "Primer Mcp "
+
+#Conección a la base de datos
+def get_conection ():
+    conn = psycopg2.connect(
+        host = os.environ.get("DB_HOST"),
+        port = os.environ.get("DB_PORT"),
+        user= os.environ.get("DB_USER"),
+        passoword= os. environ.get("DB_PASSWORD"),
+        database = os.environ.get("DB_DATABASE"),
+        cursor_factory = RealDictCursor
+    )
+    return conn
